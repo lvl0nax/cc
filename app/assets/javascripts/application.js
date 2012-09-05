c7af752d59d7e707fff55a14a6d301213af11e82
lvl0nax <lvl0nax@gmail.com>
2012-09-05 18:34:21 +0400

    dinamic adding new items

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,15 +20,24 @@ $(function() {
 	});
 
 
-	$('.add').click( function(){
+	$('.add').on("click", function(){
 		$('.create_links').show();
 		$('.add').addClass("add_select");
 		
 	});
 
+	$(document).on('click',"#close" ,function() {
+		$('.create_links').hide();
+		$('#popup-wrap').html("").removeClass("grant-popup");
+		$('.create_grant').removeClass("cr_select");
+    $('.create_event').removeClass("cr_select");
+    $('.create_training').removeClass("cr_select");
+    $('.add').removeClass("add_select");
+	});
+
 	$('.create_grant a').bind('click', function(){
-		('#popup-wrap').addClass("grant-popup");
-		$('.create_links').show();
+		$('#container').css("min-height", "900px");
+		$('#popup-wrap').addClass("grant-popup");
     $('.create_grant').addClass("cr_select");
     $('.create_event').removeClass("cr_select");
     $('.create_training').removeClass("cr_select");
@@ -37,32 +46,26 @@ $(function() {
 	});
 
 	$('.create_training a').bind('click', function(){
-		('#popup-wrap').addClass("grant-popup");
-		$('.create_links').show();
+		$('#popup-wrap').addClass("grant-popup").load("/trainings/new", function(){
+        geocoder = new google.maps.Geocoder();
+        geo.setLoc("59.93365223894488","30.300378486327617");
+        geo.init({isFirstSet: true, map: 'gm', elementString : "#geo-map"});
+		});
     $('.create_training').addClass("cr_select");
     $('.create_event').removeClass("cr_select");
     $('.create_grant').removeClass("cr_select");
-		$('#popup-wrap').load("/trainings/new", function(){
-        geocoder = new google.maps.Geocoder();
-        geo.setLoc("59.93365223894488","30.300378486327617");
-        geo.init({isFirstSet: true, map: 'gm', elementString : "#geo-map"})
-		});
 		return false;
 	});
 
 	$('.create_event a').bind('click', function(){
-		$('#popup-wrap').addClass("grant-popup");
-		$('.create_links').show();
+		$('#popup-wrap').addClass("grant-popup").load("/events/new", function(){
+        geocoder = new google.maps.Geocoder();
+        geo.setLoc("59.93365223894488","30.300378486327617");
+        geo.init({isFirstSet: true, map: 'gm', elementString : "#geo-map"});
+		});
     $('.create_event').addClass("cr_select");
     $('.create_training').removeClass("cr_select");
     $('.create_grant').removeClass("cr_select");
-		//$('#popup-wrap').load("/events/new");
-		$('#popup-wrap').load("/grants/new", function(){
-        geocoder = new google.maps.Geocoder();
-        geo.setLoc("59.93365223894488","30.300378486327617");
-        geo.init({isFirstSet: true, map: 'gm', elementString : "#geo-map"})
-		});
-
 		return false;
 	});
 

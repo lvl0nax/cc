6f684be021d3a607faea23643cfdc4aa7e164995
Davert <davert@mail.ua>
2012-11-13 03:21:10 +0200

    selectboxes implemented

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,8 @@
 //= require jquery
 //= require_tree .
 
-$(function() {
-	/*$('.trainings input:checkbox').click(function() {
-		$('.temp_month').toggle();
-	});
-*/
+$(document).ready(function() {
+
   $('#temp_month .list a').live('click', function () {
     $("#events_search input[name=year]").val($(this).data('year'));
     $("#events_search input[name=month]").val($(this).data('month'));
@@ -23,25 +20,22 @@ $(function() {
     $(this).find('.this').animate({left: '0px'}, 200);
   });
 
+  $('select').selectbox({ effect: "slide" });
 
-	$("#events_search input[type=checkbox]").bind("click", function(){$("#events_search").submit()});
+  $("#events_search input[type=checkbox]").bind("click", function(){$("#events_search").submit()});
 
-  //highlight link in login menu
-	var url=document.location.href;
-	$.each($(".login a"), function(){
-		if (this.href==url){$(this).parent().addClass('acta');}
-	});
+  var url=document.location.href;
+  $.each($(".login a"), function(){
+    if (this.href==url){$(this).parent().addClass('acta');}
+  });
 
-  // old method for old search)))
   $('#events_search').submit( function () {
     $("#items").html("");
     $.get(this.action, $(this).serialize(), null, 'script');
     return false;
   });
 
-  // click on add button
   $('.add').on("click", function(){
-
       $('.create_links').show();
       $('.add').addClass("add_select");
       $(".reg-buttons").hide();
@@ -62,11 +56,6 @@ $(function() {
     this.css("left", ($(window).width() - this.width() ) / 2 + "px");
     return this;
   }
-  /*$("#new_user").on();*/
-  /*$(document).on("ajax:error", "#new_user", function(evt, xhr, settings){alert(1);});*/
-
-
-  // for close popup window - maybe can be deleted
   $(document).on('click',".wait-click" ,function() {
     if ($("#popup-wrap").is(".show-popup")) {
       $("#popup-wrap").html("").removeClass();
@@ -75,15 +64,15 @@ $(function() {
   });
 
   //login
-	$(document).on("submit","#new_session", function(event){
-			event.preventDefault();
-			fnlogin();
-		});
+  $(document).on("submit","#new_session", function(event){
+      event.preventDefault();
+      fnlogin();
+    });
   //registration submit
-	$(document).on("submit","#new_user", function(event){
-			event.preventDefault();
-			test();
-		});
+  $(document).on("submit","#new_user", function(event){
+      event.preventDefault();
+      test();
+    });
 
   // set geo coordinate when mouse leave map-area
   $(document).on('hover',"#geo-map" ,function() {
@@ -104,144 +93,150 @@ $(function() {
   */
 
   //open popup for creating grant
-	$('.create_grant a').bind('click', function(){
-		$('#content-body').css("min-height", "900px");
-		$(".reg-buttons").hide();
+  $('.create_grant a').bind('click', function(){
+    $('#content-body').css("min-height", "900px");
+    $(".reg-buttons").hide();
     $('.create_grant').addClass("cr_select");
     $('.create_event').removeClass("cr_select");
     $('.create_training').removeClass("cr_select");
-		$('#popup-wrap').removeClass().addClass("grant-popup").load("/grants/new");
-		return false;
-	});
+    $('#popup-wrap').removeClass().addClass("grant-popup").load("/grants/new", function() {
+      $('#popup-wrap select').selectbox({ effect: "slide" });
+    });
+    return false;
+  });
   // --//-- fortraining
-	$('.create_training a').bind('click', function(){
-		$('#content-body').css("min-height", "900px");
-		$(".reg-buttons").hide();
-		$('#popup-wrap').removeClass().attr("onclick", "areahide();").addClass("grant-popup").load("/trainings/new", function(){
+  $('.create_training a').bind('click', function(){
+    $('#content-body').css("min-height", "900px");
+    $(".reg-buttons").hide();
+    $('#popup-wrap').removeClass().attr("onclick", "areahide();").addClass("grant-popup").load("/trainings/new", function(){
         geocoder = new google.maps.Geocoder();
         geo.setLoc("59.93365223894488","30.300378486327617");
         geo.init({isFirstSet: true, map: 'gm', elementString : "#geo-map"});
-		});
+        $('#popup-wrap select').selectbox({ effect: "slide" });        
+    });
     $('.create_training').addClass("cr_select");
     $('.create_event').removeClass("cr_select");
     $('.create_grant').removeClass("cr_select");
-		return false;
-	});
+    
+
+    return false;
+  });
   // for event
-	$('.create_event a').bind('click', function(){
-		$('#content-body').css("min-height", "900px");
-		$(".reg-buttons").hide();
-		$('#popup-wrap').removeClass().attr("onclick", "areahide();").addClass("grant-popup").load("/events/new", function(){
+  $('.create_event a').bind('click', function(){
+    $('#content-body').css("min-height", "900px");
+    $(".reg-buttons").hide();
+    $('#popup-wrap').removeClass().attr("onclick", "areahide();").addClass("grant-popup").load("/events/new", function(){
         geocoder = new google.maps.Geocoder();
         geo.setLoc("59.93365223894488","30.300378486327617");
         geo.init({isFirstSet: true, map: 'gm', elementString : "#geo-map"});
-		});
+    });
     $('.create_event').addClass("cr_select");
     $('.create_training').removeClass("cr_select");
     $('.create_grant').removeClass("cr_select");
-		return false;
-	});
+    $('#new_event select').selectbox({ effect: "slide" });
+    return false;
+  });
 
   // open registration form
-	$('.registration').bind('click', function(){
-		
+  $('.registration').bind('click', function(){
+    
     if ($(this).data().reg){alert("Для регистрации разлогиньтесь, пожалуйста.")} 
     else {
-    	$('#login-form').html("");
-    	$('.error').html("");
-  		$(this).addClass("reg-select");
-  		$('.create_links').hide();
-  		$(".reg-buttons").show();
-  		$('#popup-wrap').removeClass().addClass("reg-popup").load("/users/sign_up", function(){
-		  			$("#new_user").validate({
-							rules: {
-								"user[email]": {
-									required: true,
-									email: true
-								},
-								"user[password]": {
-									required: true,
-									minlength: 6
-								},
-								"user[password_confirmation]": {
-									required: true,
-									minlength: 6,
-									equalTo: "#user_password"
-								}
-							},
-							messages:{
-								"user[email]": {
-									required: "Введите Email",
-									email: "Email некорректный"
-								},
-								"user[password]": {
-									required: "Введите пароль",
-									minlength: "Пароль должен быть не меньше 6 символов"
-								},
-								"user[password_confirmation]": {
-									required: "Потвторите пороль",
-									minlength: "Пароль должен быть не меньше 6 символов",
-									equalTo: "Пароли не совпадают"
-								}
-							}
-						});
-  		});
-  	
+      $('#login-form').html("");
+      $('.error').html("");
+      $(this).addClass("reg-select");
+      $('.create_links').hide();
+      $(".reg-buttons").show();
+      $('#popup-wrap').removeClass().addClass("reg-popup").load("/users/sign_up", function(){
+            $("#new_user").validate({
+              rules: {
+                "user[email]": {
+                  required: true,
+                  email: true
+                },
+                "user[password]": {
+                  required: true,
+                  minlength: 6
+                },
+                "user[password_confirmation]": {
+                  required: true,
+                  minlength: 6,
+                  equalTo: "#user_password"
+                }
+              },
+              messages:{
+                "user[email]": {
+                  required: "Введите Email",
+                  email: "Email некорректный"
+                },
+                "user[password]": {
+                  required: "Введите пароль",
+                  minlength: "Пароль должен быть не меньше 6 символов"
+                },
+                "user[password_confirmation]": {
+                  required: "Потвторите пороль",
+                  minlength: "Пароль должен быть не меньше 6 символов",
+                  equalTo: "Пароли не совпадают"
+                }
+              }
+            });
+      });
+    
     }
-	});
-	
+  });
+  
   //select role
-	$('.person-button').bind("click", function(){
-		$('#user_role').val("employee");
-		$(this).addClass("rselected");
-		$('.comp-button').removeClass("rselected");
-	});
-	$('.comp-button').bind("click", function(){
-		$('#user_role').val("employer");
-		$(this).addClass("rselected");
-		$('.person-button').removeClass("rselected");
-	});
+  $('.person-button').bind("click", function(){
+    $('#user_role').val("employee");
+    $(this).addClass("rselected");
+    $('.comp-button').removeClass("rselected");
+  });
+  $('.comp-button').bind("click", function(){
+    $('#user_role').val("employer");
+    $(this).addClass("rselected");
+    $('.person-button').removeClass("rselected");
+  });
 
   //hide area and company dropdown
-	$('#container').bind('click', function () {
-		$(".ard").fadeOut();
-		$(".arde").fadeOut();
-		$(".ardc").fadeOut();
-		/*if ($("#popup-wrap").is(".show-popup")) 
-			$("#popup-wrap").removeClass();*/
-	});
+  $('#container').bind('click', function () {
+    $(".ard").fadeOut();
+    $(".arde").fadeOut();
+    $(".ardc").fadeOut();
+    /*if ($("#popup-wrap").is(".show-popup")) 
+      $("#popup-wrap").removeClass();*/
+  });
 
   // company or area dropdown
-	$('#trclick').bind('click', function () {
-		$('.ard').fadeIn();
-		$(".arde").fadeOut();
-		$(".ardc").fadeOut();
-	});
-	$('#trcclick').bind('click', function () {
-		$('.ardc').fadeIn();
-		$(".ard").fadeOut();
-		$(".arde").fadeOut();
-	});
-	$('#evclick').bind('click', function () {
-		$('.arde').fadeIn();
-		$(".ard").fadeOut();
-		$(".ardc").fadeOut();
-	});
+  $('#trclick').bind('click', function () {
+    $('.ard').fadeIn();
+    $(".arde").fadeOut();
+    $(".ardc").fadeOut();
+  });
+  $('#trcclick').bind('click', function () {
+    $('.ardc').fadeIn();
+    $(".ard").fadeOut();
+    $(".arde").fadeOut();
+  });
+  $('#evclick').bind('click', function () {
+    $('.arde').fadeIn();
+    $(".ard").fadeOut();
+    $(".ardc").fadeOut();
+  });
 
-	$(".select").click(function(event){ event.stopPropagation()});
+  $(".select").click(function(event){ event.stopPropagation()});
 
-	$(".select div.input input.check_boxes").bind("click", function(){
-		if ($(this).attr('checked'))
-		{
-			$(this).attr('checked', 'checked').parent().css('color', '#76a38a');
+  $(".select div.input input.check_boxes").bind("click", function(){
+    if ($(this).attr('checked'))
+    {
+      $(this).attr('checked', 'checked').parent().css('color', '#76a38a');
 
-		}else{
-			
-			$(this).removeAttr('checked').parent().css('color', 'black');
-		}
-		
+    }else{
+      
+      $(this).removeAttr('checked').parent().css('color', 'black');
+    }
+    
 
-	});
+  });
 
   $("#arealist input[type=checkbox]").each(function(){
     if ($(this).attr("checked") ){ 
@@ -249,25 +244,25 @@ $(function() {
     }
   });
 /*
-	$(".select label.checkbox").bind("click", function(){
-		if ($(this).children.attr("ckecked"))
-			{
-				$(this).css("color", "#76a38a");
-			}
-		else
-			{
-				$(this).css("color", "black");
-			}
-	});
+  $(".select label.checkbox").bind("click", function(){
+    if ($(this).children.attr("ckecked"))
+      {
+        $(this).css("color", "#76a38a");
+      }
+    else
+      {
+        $(this).css("color", "black");
+      }
+  });
 
 */
 
   //login form load
-	$("#login").bind("click", function(){
-		closeItemPopup();
-		$("#login-form").load("/users/sign_in");
-		return false;
-	});
+  $("#login").bind("click", function(){
+    closeItemPopup();
+    $("#login-form").load("/users/sign_in");
+    return false;
+  });
 
   // for checkbox style
   var temp = $(".menu label.checkbox input");
@@ -302,10 +297,10 @@ $(function() {
   
 });/*
 $('.trainings input:checkbox').click(function() {
-	if (this.checked)
-		$(".temp_event").show();
-	else
-  	$(".temp_event").hide();
+  if (this.checked)
+    $(".temp_event").show();
+  else
+    $(".temp_event").hide();
 });*/
 
 // for close any popup 
@@ -323,40 +318,40 @@ function closeItemPopup() {
 
 //velidation register form - need refactoring with above code validation
 function formvalidate() {
-	$("#new_user").validate({
-		rules: {
-			"user[email]": {
-				required: true,
-				email: true
-			},
-			"user[password]": {
-				required: true,
-				minlength: 6
-			},
-			"user[password_confirmation]": {
-				required: true,
-				minlength: 6,
-				equalTo: "#user_password"
-			}
-		},
-		messages:{
-			"user[email]": {
-				required: "Введите Email",
-				email: "Email некорректный"
-			},
-			"user[password]": {
-				required: "Введите пароль",
-				minlength: "Пароль должен быть не меньше 6 символов"
-			},
-			"user[password_confirmation]": {
-				required: "Потвторите пороль",
-				minlength: "Пароль должен быть не меньше 6 символов",
-				equalTo: "Пароли не совпадают"
-			}
-		}
-	});
+  $("#new_user").validate({
+    rules: {
+      "user[email]": {
+        required: true,
+        email: true
+      },
+      "user[password]": {
+        required: true,
+        minlength: 6
+      },
+      "user[password_confirmation]": {
+        required: true,
+        minlength: 6,
+        equalTo: "#user_password"
+      }
+    },
+    messages:{
+      "user[email]": {
+        required: "Введите Email",
+        email: "Email некорректный"
+      },
+      "user[password]": {
+        required: "Введите пароль",
+        minlength: "Пароль должен быть не меньше 6 символов"
+      },
+      "user[password_confirmation]": {
+        required: "Потвторите пороль",
+        minlength: "Пароль должен быть не меньше 6 символов",
+        equalTo: "Пароли не совпадают"
+      }
+    }
+  });
 
-	test();
+  test();
 
 }
 
@@ -371,15 +366,15 @@ function test () {
       if (data.match("Email is already taken")) {
         alert("Данный email уже используется")
       } else {
-      	if (data.match("ПАРОЛЬ ЕЩЕ РАЗ")) {
-	        alert("Что-то пошло не так. Проверьте ваши данные и попробуйте еще раз.")
-	      } else {
-	      	if ($("#user_role").val() == "employer"){
-	        	location.replace("/compinfos/new");
-	      	}else{
-	        	location.replace("/resumes/new");
-	      	}
-      	}	
+        if (data.match("ПАРОЛЬ ЕЩЕ РАЗ")) {
+          alert("Что-то пошло не так. Проверьте ваши данные и попробуйте еще раз.")
+        } else {
+          if ($("#user_role").val() == "employer"){
+            location.replace("/compinfos/new");
+          }else{
+            location.replace("/resumes/new");
+          }
+        } 
       }
     },
     error: function(){ alert(2); }
@@ -396,12 +391,12 @@ function fnlogin () {
     data: $("#new_session").serialize(),
     
     success: function(data, status, jqXHR){ 
-    	location.reload();
+      location.reload();
     },
     error: function(jqXHR, textStatus, error){ 
-    	$(".error").html("").append("Пожалуйста, проверьте введенные данные и повторите попытку снова.");
-    	$("#user_email").addClass("red-border");
-    	$("#user_password").addClass("red-border");
+      $(".error").html("").append("Пожалуйста, проверьте введенные данные и повторите попытку снова.");
+      $("#user_email").addClass("red-border");
+      $("#user_password").addClass("red-border");
     }
     
   });
@@ -411,14 +406,14 @@ function fnlogin () {
 function areashow(){
   $(".select").click(function(event){ event.stopPropagation()});
   $("#evcreateclick").click(function(event){ event.stopPropagation()});
-	$('.ardec').fadeIn();
+  $('.ardec').fadeIn();
 
-	/*$('#evcreateclick').bind('click', function () {
-		$('.ardec').fadeIn();
-	});
-	$('#new_event').bind('click', function () {
-		$('.ardec').fadeIn();
-	});*/
+  /*$('#evcreateclick').bind('click', function () {
+    $('.ardec').fadeIn();
+  });
+  $('#new_event').bind('click', function () {
+    $('.ardec').fadeIn();
+  });*/
 }
 function areahide(){
   $(".select").click(function(event){ event.stopPropagation()});
@@ -430,7 +425,7 @@ function areahide(){
 
 //show items  - event/training/grant
 function showevent(temp){
-		closeItemPopup();
+    closeItemPopup();
     scrollPopup();
 
     $("#popup-wrap").click(function(event){ event.stopPropagation()});
@@ -453,7 +448,7 @@ function showevent(temp){
 }
 function showtraining(temp){
 
-  	closeItemPopup();
+    closeItemPopup();
     scrollPopup();
     $("#popup-wrap").click(function(event){ event.stopPropagation()});
 
@@ -485,7 +480,7 @@ function scrollPopup()
 }
 
 function showgrant(temp){
-		closeItemPopup();
+    closeItemPopup();
     scrollPopup();
 
     $("#popup-wrap").click(function(event){ event.stopPropagation()});
@@ -517,79 +512,79 @@ function itemshow(sclass){
 
 //for search items
 function srch() {
-	/*var t = $("#events_search").serialize();
-	alert(t);*/
+  /*var t = $("#events_search").serialize();
+  alert(t);*/
 
-	$.ajax({
+  $.ajax({
     type: "POST",
     url: "/events",
     data: $("#events_search").serialize(),
     
     success: function(data, status, jqXHR){ 
-    	/*location.reload();*/
-		  //	$('#items').html();
-		  //	$('#items').append("<%= j render(@items) %>");
-			// /*if (@items.length > 12) {*/
-			// 	$(".pagination").replaceWith("<%= j will_paginate(@items) %>");
-			// /*}
-			// else{
-			// 	$(".pagination").remove();
-			// }*/
+      /*location.reload();*/
+      //  $('#items').html();
+      //  $('#items').append("<%= j render(@items) %>");
+      // /*if (@items.length > 12) {*/
+      //  $(".pagination").replaceWith("<%= j will_paginate(@items) %>");
+      // /*}
+      // else{
+      //  $(".pagination").remove();
+      // }*/
     },
     error: function(jqXHR, textStatus, error){ 
-    	alert("status " + textStatus);
+      alert("status " + textStatus);
     }
   });
 }
 
 //adding participant to events/training/grant
 function addpart(){
-	var url = $(".participant div").data("url");
-	var id = $(".participant div").data("id");
-	var item = $(".participant div").data("item");
-	$.ajax({
+  var url = $(".participant div").data("url");
+  var id = $(".participant div").data("id");
+  var item = $(".participant div").data("item");
+  $.ajax({
     type: "GET",
     url: url,
     /*data: id,*/
     /*success: function(){ 
-    	alert("tests1");
+      alert("tests1");
     },
     error: function(jqXHR, textStatus, errorThrown){ 
-    	alert("error ");
-    	
+      alert("error ");
+      
     }*/
     complete: function(){
-    	if (item == "grant"){
-    		showgrant(id);
-    	}else if (item == "event"){
-    		showevent(id)
-    	}else {
-    		showtraining(id)
-    	}
+      if (item == "grant"){
+        showgrant(id);
+      }else if (item == "event"){
+        showevent(id)
+      }else {
+        showtraining(id)
+      }
     }
   });
 }
 
 //delete participant
 function delpart(){
-	var url = $(".participant div").data("url");
-	var id = $(".participant div").data("id");
-	var item = $(".participant div").data("item");
-	$.ajax({
+  var url = $(".participant div").data("url");
+  var id = $(".participant div").data("id");
+  var item = $(".participant div").data("item");
+  $.ajax({
     type: "GET",
     url: url,
     data: id,
     success: function(){ 
-    	if (item == "grant"){
-    		showgrant(id);
-    	}else if (item == "event"){
-    		showevent(id)
-    	}else {
-    		showtraining(id)
-    	}
+      if (item == "grant"){
+        showgrant(id);
+      }else if (item == "event"){
+        showevent(id)
+      }else {
+        showtraining(id)
+      }
     },
     error: function(jqXHR, textStatus, errorThrown){ 
-    	alert(errorThrown);
+      alert(errorThrown);
     }
   });
 }
